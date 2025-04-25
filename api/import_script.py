60c0d4f9617d592d3ade1ae123f444f5d0e299db
Emi004 <aenoaeiemi49@gmail.com>
2025-04-25 19:02:37 +0300

    make populate function

--- api/import_script.py
+++ api/import_script.py
@@ -2,6 +2,21 @@ import csv
 import json
 import re
 
+from unicodedata import category
+
+from app import app, db
+from models.recipe import Recipe
+from models.ingredient import Ingredient
+from models.category import Category
+
+CATEGORY_COLORS = {
+    'Baking': '#FFA500',  # orange
+    'Cookies': '#D2691E',  # chocolate
+    'Pie': '#FFD700',  # gold
+    'Italian': '#FF0000',  # red
+    'No-bake': '#ADD8E6'  # lightblue
+}
+
 
 def get_all_recipes():
     ingredient_regex = re.compile(r'^(?P<quantity>\d+)(?P<unit>[a-zA-Z]*) (?P<name>.+)$')
@@ -34,5 +49,48 @@ def get_all_recipes():
         return all_recipes
 
 
+def populate_db(recipes):
+    with app.app_context():
+        try:
+            for recipe_data in recipes:
+                category_obj = []
+                for cat_name in recipe_data["Categories"]:
+                    category = Category.query.filter_by(name=cat_name).first()
+                    if not category:
+                        category = Category(name=cat_name, color=CATEGORY_COLORS.get(cat_name, '#0f0f0f'))
+                        db.session.add(category)
+                    category_obj.append(category)
+                # string = ''
+                # for pic in recipe_data["Pictures"]:
+                #     string = string + pic + ','
+                # VERBOUSSSSSSSSSS!!!!!!
+                recipe = Recipe(name=recipe_data["Recipe name"],
+                                duration=recipe_data["Duration"],
+                                pictures=','.join(recipe_data["Pictures"]),
+                                instructions=recipe_data["Instructions"],
+                                categories=category_obj
+                                )
+                db.session.add(recipe)
+                db.session.flush()
+
+                for ing in recipe_data["Ingredients"]:
+                    ingredient = Ingredient(
+                        name=ing['name'],
+                        unit=ing['unit'],
+                        quantity=ing['quantity'],
+                        recipe_id=recipe.id
+
+                    )
+                    db.session.add(ingredient)
+
+                print(f'Inserted recipe:{recipe.name}')
+            db.session.commit()
+            print("populated db")
+        except Exception as e:
+            db.session.rollback()
+            print(e)
+
+
 if __name__ == '__main__':
     recipes = get_all_recipes()
+    populate_db(recipes)
